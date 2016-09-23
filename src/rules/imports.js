36a3fe3898246be8e1af1e4681a16d5082672adf
Alexander Antipin <alex@antipin.com>
2016-09-23 17:58:40 +0300

    Change import/no-extraneous-dependencies.devDependencies rule from false to true

--- src/rules/imports.js
+++ src/rules/imports.js
@@ -69,7 +69,7 @@ module.exports = {
         // Forbid the use of extraneous packages
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
         'import/no-extraneous-dependencies': [ ERROR, {
-            devDependencies: false,
+            devDependencies: true,
             optionalDependencies: false,
         } ],
 
